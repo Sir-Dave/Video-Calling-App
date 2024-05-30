e205270e9d6b588bb07c238c1e287dfae4ece9b1
Abiola David <davidolanrewaju2000@gmail.com>
2024-05-30 15:41:06 +0100

    Initialized project && git brach -M main

---
+++ app/src/main/java/com/sirdave/videostreamapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.sirdave.videostreamapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sirdave.videostreamapp.ui.theme.VideoStreamAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VideoStreamAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    VideoStreamAppTheme {
+        Greeting("Android")
+    }
+}
