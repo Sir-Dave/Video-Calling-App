b2e19e34c5031972291060ea37c5c8746d0fbb19
Abiola David <davidolanrewaju2000@gmail.com>
2024-05-30 20:18:18 +0100

    Configured Stream SDK in Application

---
+++ app/src/main/java/com/sirdave/videostreamapp/VideoStreamApp.kt
@@ -0,0 +1,31 @@
+package com.sirdave.videostreamapp
+
+import android.app.Application
+import io.getstream.video.android.core.StreamVideo
+import io.getstream.video.android.core.StreamVideoBuilder
+import io.getstream.video.android.model.User
+import io.getstream.video.android.model.UserType
+
+class VideoStreamApp: Application() {
+    private var currentName: String? = null
+    var client: StreamVideo? = null
+
+
+    fun initVideoClient(username: String){
+        if (client == null || username != currentName){
+            StreamVideo.removeClient()
+            currentName = username
+
+            client = StreamVideoBuilder(
+                context = this,
+                apiKey = BuildConfig.STREAM_API_KEY,
+                user = User(
+                    id = username,
+                    name = username,
+                    type = UserType.Guest
+                )
+            ).build()
+        }
+
+    }
+}
